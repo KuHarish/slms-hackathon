ed0779de366c36c8892ec564f4b7f2347bc47ae9
KuHarish <hnthilagar@gmail.com>
2026-02-26 02:54:44 +0530

    Added Modules and Routes

---
+++ backend/models/Book.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+    
+    title:{
+        type: String,
+        required: true
+    },
+    author:{
+        type: String,
+        required: true
+    },
+    description: String,
+    totalCopies:{
+        type: Number,
+        required: true
+    },
+    availableCopies:{
+        type: Number,
+        required: true
+    }
+    },{ timestamps: true});
+
+module.exports = mongoose.model("Book", bookSchema);
