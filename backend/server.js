dd91689d431c5853390bb26832c27e3881851046
KuHarish <hnthilagar@gmail.com>
2026-03-06 21:38:51 +0530

    feat: Initialize backend server with Express, MongoDB, and API routes for books, notifications, reviews, and authentication.

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ const bookRoutes = require("./routes/bookRoutes");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -29,6 +28,7 @@ app.get("/", (req, res) => {
   res.send("API Working");
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
